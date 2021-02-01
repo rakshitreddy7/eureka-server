18976d78bd2e75295d54ef0f498ace6c4d2f99d0
rakshitreddy7 <rakshitreddy7@gmail.com>
2021-02-01 17:58:41 -0600

    Renaming

--- src/main/java/com/service/discovery/discoverserver/DiscoveryServiceApplication.java
+++ src/main/java/com/service/discovery/discoverserver/DiscoveryServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class DiscoverServerApplication {
+public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiscoverServerApplication.class, args);
+		SpringApplication.run(DiscoveryServiceApplication.class, args);
 	}
 
 }
--- src/test/java/com/service/discovery/discoverserver/DiscoveryServiceApplicationTests.java
+++ src/test/java/com/service/discovery/discoverserver/DiscoveryServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DiscoverServerApplicationTests {
+class DiscoveryServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
